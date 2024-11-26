625089affe54fe19f0573494268e20c1d5225537
Tarun Prabhu <tarun.prabhu@gmail.com>
2024-11-26 16:45:52 -0700

    [Kitsune] More implementation
    The test suite configures at least. Not all combinations of options have been
    tested.

--- cmake/modules/KitsuneTestSuite.cmake
+++ cmake/modules/KitsuneTestSuite.cmake
@@ -7,23 +7,47 @@
 
 include(SingleMultiSource)
 
+# Look at the file name to determine the source language. This is not really a
+# source language in the strict sense of the term, but something for internal
+# use that will help determine what to do with the file. The "not widely
+# recognized as languages" languages that this will return are:
+#
+#    kitc       C files with kitsune-specific extensions
+#    kitc++     C++ files with kitsune-specific extensions
+#    kokkos     C++ files with Kokkos. These may or may not contain any
+#               Kitsune-specific extensions
+#
+# The "recognized as a language by a different name" are:
+#
+#    cuda-lang  Cuda files (those with a .cu extension)
+#    hip-lang   Hip files (those with a .hip extension)
+#
+# These are done to distinguish them from the cuda and hip tapir targets.
+#
+# The other source languages that this will return are c, c++, and fortran
+# which are all what one might expect.
+#
 function (source_language source lang)
   # We treat Kokkos as its own language.
-  if (${source} MATCHES ".+-kokkos[.].+$")
+  if (source MATCHES ".+[.]kokkos[.]cpp$" OR source MATCHES ".+[.]kokkos[.]c$")
     set(${lang} "kokkos" PARENT_SCOPE)
-  elseif (${source} MATCHES ".+[.]c$")
+  elseif (source MATCHES ".+[.]kit[.]c$" OR source MATCHES ".+[.]kit[.]c$")
+    set(${lang} "kitc" PARENT_SCOPE)
+  elseif (source MATCHES ".+[.]kit[.]cpp$" OR source MATCHES ".+[.]kit[.]cc$")
+    set(${lang} "kitc++" PARENT_SCOPE)
+  elseif (source MATCHES ".+[.]c$")
     set(${lang} "c" PARENT_SCOPE)
-  elseif (${source} MATCHES ".+[.]cpp$" OR ${source} MATCHES ".+[.]cc$")
+  elseif (source MATCHES ".+[.]cpp$" OR source MATCHES ".+[.]cc$")
     set(${lang} "c++" PARENT_SCOPE)
-  elseif (${source} MATCHES ".+[.]cu$")
-    set(${lang} "cuda" PARENT_SCOPE)
-  elseif (${source} MATCHES ".+[.]hip$")
-    set(${lang} "hip" PARENT_SCOPE)
-  elseif (${source} MATCHES ".+[.][Ff]$" OR
-      ${source} MATCHES ".+[.][Ff]90$" OR
-      ${source} MATCHES ".+[.][Ff]95$" OR
-      ${source} MATCHES ".+[.][Ff]03$" OR
-      ${source} MATCHES ".+[.][Ff]08$")
+  elseif (source MATCHES ".+[.]cu$")
+    set(${lang} "cuda-lang" PARENT_SCOPE)
+  elseif (source MATCHES ".+[.]hip$")
+    set(${lang} "hip-lang" PARENT_SCOPE)
+  elseif (source MATCHES ".+[.][Ff]$" OR
+      source MATCHES ".+[.][Ff]90$" OR
+      source MATCHES ".+[.][Ff]95$" OR
+      source MATCHES ".+[.][Ff]03$" OR
+      source MATCHES ".+[.][Ff]08$")
     set(${lang} "fortran" PARENT_SCOPE)
   else ()
     message(FATAL_ERROR "Cannot determine source language: ${source}")
@@ -39,28 +63,69 @@ function (kitsune_singlesource_test source lang tapir_target)
   endif ()
   set(test_name "${target}")
 
+  message(STATUS "Setting up test: ${target}")
   llvm_test_executable_no_test(${target} ${source})
   llvm_test_traditional(${target})
   llvm_add_test_for_target(${target})
 
   set_property(TARGET ${target} PROPERTY TEST_NAME "${test_name}")
-  if (${lang} STREQUAL "cuda")
+  if (lang STREQUAL "c")
+    target_compile_options(${target} PUBLIC
+      "${CMAKE_C_FLAGS}")
+  elseif (lang STREQUAL "c++")
+    target_compile_options(${target} PUBLIC
+      "${CMAKE_CXX_FLAGS}")
+  elseif (lang STREQUAL "cuda")
     set_target_properties(${target} PROPERTIES
       CUDA_COMPILER_LAUNCHER ${NVCC})
-  elseif (${lang} STREQUAL "hip")
+  elseif (lang STREQUAL "hip")
     set_target_properties(${target} PROPERTIES
       HIP_COMPILER_LAUNCHER ${HIPCC})
-  elseif (${lang} STREQUAL "c")
+  elseif (lang STREQUAL "fortran")
     target_compile_options(${target} PUBLIC
-      "-ftapir=${tapir_target}" "-fno-exceptions" "${CMAKE_C_FLAGS}")
-  elseif (${lang} STREQUAL "c++")
+      "-ftapir=${tapir_target}")
+  elseif (lang STREQUAL "kitc")
     target_compile_options(${target} PUBLIC
-      "-ftapir=${tapir_target}" "-fno-exceptions" "${CMAKE_CXX_FLAGS}")
-  elseif (${lang} STREQUAL "fortran")
+      "-ftapir=${tapir_target}")
+  elseif (lang STREQUAL "kitc++")
     target_compile_options(${target} PUBLIC
-      "-ftapir=${tapir_target}" "-fno-exceptions" "${CMAKE_Fortran_FLAGS}")
+      "-ftapir=${tapir_target}" "-fno-exceptions")
+  elseif (lang STREQUAL "kokkos")
+    target_compile_options(${target} PUBLIC
+      "-fkokkos" "-fkokkos-no-init" "-ftapir=${tapir_target}" "-fno-exceptions")
   else ()
     message(FATAL_ERROR "Unsupported language: ${lang} [${source}]")
+  endif ()
+endfunction()
+
+# Setup a single source test for all tapir targets that are being tested.
+function(kitsune_singlesource_setup source lang)
+  if (TEST_CUDA_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "cuda")
+  endif ()
+  if (TEST_HIP_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "hip")
+  endif()
+  if (TEST_LAMBDA_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "lambda")
+  endif ()
+  if (TEST_OMPTASK_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "omptask")
+  endif ()
+  if (TEST_OPENCILK_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "opencilk")
+  endif()
+  if (TEST_OPENMP_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "openmp")
+  endif ()
+  if (TEST_QTHREADS_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "qthreads")
+  endif ()
+  if (TEST_REALM_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "realm")
+  endif ()
+  if (TEST_SERIAL_TARGET)
+    kitsune_singlesource_test(${source} ${lang} "serial")
   endif ()
 endfunction()
 
@@ -74,51 +139,46 @@ function(kitsune_singlesource)
     *.f *.F *.f90 *.F90 *.f03 *.F03 *.f08 *.F08
     *.hip)
   foreach(source ${sources})
-    set(is_kokkos_source OFF)
-    set(is_cuda_source OFF)
-    set(is_hip_source OFF)
-
     set(lang)
     source_language(${source} lang)
 
     # Fortran is not yet supported. Complain loudly so we know to change this
     # and take a closer look when Fortran is supported.
-    if (${lang} STREQUAL "fortran")
+    if (lang STREQUAL "fortran")
       message(FATAL_ERROR "Kitsune does not yet support Fortran: ${source}")
     endif ()
 
-    if (${lang} STREQUAL "cuda" AND ${TEST_CUDA_LANG})
-      kitsune_singlesource_test(${source} "cuda-lang" "none")
-    elseif (${lang} STREQUAL "hip" AND ${TEST_HIP_LANG})
-      kitsune_singlesource_test(${source} "hip-lang" "none")
+    if (lang STREQUAL "cuda-lang")
+      if (TEST_CUDA_LANG)
+        kitsune_singlesource_test(${source} ${lang} "none")
+      endif ()
+    elseif (lang STREQUAL "hip-lang")
+      if (TEST_HIP_LANG)
+        kitsune_singlesource_test(${source} ${lang} "none")
+      endif ()
+    elseif (lang STREQUAL "kokkos")
+      if (TEST_KOKKOS_LANG)
+        # This should be tested with a regular C++ compiler, so change the
+        # language to C++, otherwise, it will activate -fkokkos-mode in Kitsune.
+        kitsune_singlesource_test(${source} "c++" "none")
+      endif ()
+      if (TEST_KOKKOS_MODE)
+        kitsune_singlesource_setup(${source} ${lang})
+      endif ()
+    elseif (lang STREQUAL "kitc")
+      if (TEST_C)
+        kitsune_singlesource_setup(${source} ${lang})
+      endif ()
+    elseif (lang STREQUAL "kitc++" AND TEST_CXX)
+      if (TEST_CXX)
+        kitsune_singlesource_setup(${source} ${lang})
+      endif ()
+    elseif (lang STREQUAL "fortran" AND TEST_Fortran)
+      if (TEST_Fortran)
+        kitsune_singlesource_setup(${source} ${lang})
+      endif ()
     else ()
-      if (TEST_CUDA_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "cuda")
-      endif ()
-      if (TEST_HIP_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "hip")
-      endif()
-      if (TEST_LAMBDA_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "lambda")
-      endif ()
-      if (TEST_OMPTASK_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "omptask")
-      endif ()
-      if (TEST_OPENCILK_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "opencilk")
-      endif()
-      if (TEST_OPENMP_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "openmp")
-      endif ()
-      if (TEST_QTHREADS_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "qthreads")
-      endif ()
-      if (TEST_REALM_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "realm")
-      endif ()
-      if (TEST_SERIAL_TARGET)
-        kitsune_singlesource_test(${source} ${lang} "serial")
-      endif ()
+      message(FATAL_ERROR "Testing of file not supported: ${source}")
     endif ()
   endforeach()
 endfunction()
