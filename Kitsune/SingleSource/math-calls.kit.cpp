19d807af6cc65b5c9ce149e578e903c87c7e526a
Tarun Prabhu <tarun@lanl.gov>
2024-12-24 12:46:27 -0700

    Fix SingleSource tests to actually check that the result is correct. Instead of using srad for the launch attribute test, just use a simple vecadd.

--- Kitsune/SingleSource/math-calls.kit.cpp
+++ Kitsune/SingleSource/math-calls.kit.cpp
@@ -1,69 +1,72 @@
-// See the README file for details.
-#include <chrono>
+// Test that calls to math functions in forall loops work correctly. This is
+// only really relevant for GPU tapir targets
+
 #include <cmath>
-#include <fstream>
 #include <iostream>
 #include <kitsune.h>
 
 using namespace kitsune;
-using namespace std;
-
-const size_t DEFAULT_ARRAY_SIZE = 1024 * 1024 * 128;
 
 struct testit {
   float a, b;
 };
 
-template <typename T> void random_fill(mobile_ptr<T> data, size_t N) {
-  for (size_t i = 0; i < N; ++i)
+template <typename T> static void random_fill(mobile_ptr<T> data, size_t n) {
+  for (size_t i = 0; i < n; ++i)
     data[i] = (2.0 * 3.142) * (rand() / (T)RAND_MAX);
 }
 
-__attribute__((noinline)) void struct_test(testit *ti) {
+__attribute__((noinline)) static void struct_test(testit *ti) {
   ti->a = 4;
   ti->b = ti->a + 4;
 }
 
-template <typename T> __attribute__((noinline)) T math_call(T value) {
+template <typename T> __attribute__((noinline)) static T math_call1(T value) {
   testit t;
   struct_test(&t);
   return fminf(value, 1234.56 - t.a + t.b);
 }
 
-template <typename T> __attribute__((noinline)) T math_call2(T value) {
+template <typename T> __attribute__((noinline)) static T math_call2(T value) {
   return sqrtf(value);
 }
 
-template <typename T>
-void parallel_work(mobile_ptr<T> dst, const mobile_ptr<T> src, int N) {
-  forall(size_t i = 0; i < N; i++) {
-    dst[i] = fminf(math_call(src[i]) + math_call2(src[i]), 100.0);
-  }
-}
-
-void print(mobile_ptr<float> data, size_t N) {
-  for (size_t i = 0; i < N; i++)
-    printf("%f  ", data[i]);
-  printf("\n\n");
+static size_t check(const mobile_ptr<float> dst, const mobile_ptr<float> src,
+                    size_t n) {
+  float epsilon = 1e-12;
+  size_t errors = 0;
+  for (size_t i = 0; i < n; ++i)
+    if (fabs(dst[i] - fminf(math_call1(src[i]) + math_call2(src[i]), 100.0)) >
+        epsilon)
+      errors += 1;
+  return errors;
 }
 
 int main(int argc, char **argv) {
-  size_t array_size = DEFAULT_ARRAY_SIZE;
-  if (argc >= 2)
-    array_size = atol(argv[1]);
-  fprintf(stdout, "array size: %ld\n", array_size);
+  size_t size = 1024 * 1024;
+  if (argc > 1)
+    size = atol(argv[1]);
 
-  mobile_ptr<float> data0(array_size);
-  mobile_ptr<float> data1(array_size);
-  random_fill(data0, array_size);
-  print(data0, 10);
+  mobile_ptr<float> dst(size);
+  mobile_ptr<float> src(size);
 
-  auto start = chrono::steady_clock::now();
-  parallel_work(data1, data0, array_size);
-  print(data1, 10);
-  auto end = chrono::steady_clock::now();
+  random_fill(dst, size);
 
-  cout << "Execution time: " << chrono::duration<double>(end - start).count()
-       << endl;
-  return 0;
+  // clang-format off
+  forall(size_t i = 0; i < size; ++i) {
+    dst[i] = fminf(math_call1(src[i]) + math_call2(src[i]), 100.0);
+  }
+  // clang-format on
+
+  std::cout << "\n  Checking final result..." << std::flush;
+  size_t errors = check(dst, src, size);
+  if (errors)
+    std::cout << "  FAIL! (" << errors << " errors found)\n\n";
+  else
+    std::cout << "  pass\n\n";
+
+  dst.free();
+  src.free();
+
+  return errors;
 }
